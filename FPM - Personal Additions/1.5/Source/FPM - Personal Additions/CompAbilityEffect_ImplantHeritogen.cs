02fa118831307bc7d6b660f6d69dbb6fc371caba
Fals3-Pr0ph3t <66283967+Fals3-Pr0ph3t@users.noreply.github.com>
2024-08-20 11:18:43 -0500

    Initial Upload

---
+++ FPM - Personal Additions/1.5/Source/FPM - Personal Additions/CompAbilityEffect_ImplantHeritogen.cs
@@ -0,0 +1,21 @@
+﻿using RimWorld;
+using Verse;
+
+namespace FPM_Additions
+{
+    public class CompAbilityEffect_ImplantHeritogen : CompAbilityEffect
+    {
+        public new AbilityCompProperties_ImplantHeritogen Props => (AbilityCompProperties_ImplantHeritogen)props;
+
+        public override void Apply(LocalTargetInfo target, LocalTargetInfo dest)
+        {
+            base.Apply(target, dest);
+            if (target.Pawn != null)
+            {
+                Pawn pawn = target.Pawn;
+            }
+            bool xenogene = Props.heritogenDef.xenogene;
+            FPM_Utilites.ImplantHeritogen(parent.pawn, target.Pawn, Props.heritogenDef);
+        }
+    }
+}
---
+++ FPM - Personal Additions/1.5/Source/FPM - Personal Additions/CompProperties_ImplantHeritogen.cs
@@ -0,0 +1,17 @@
+﻿using RimWorld;
+
+namespace FPM_Additions
+{
+
+
+    public class AbilityCompProperties_ImplantHeritogen : CompProperties_AbilityEffect
+    {
+        public HeritogenDef heritogenDef;
+
+
+        public AbilityCompProperties_ImplantHeritogen()
+        {
+            compClass = typeof(CompAbilityEffect_ImplantHeritogen);
+        }
+    }
+}
