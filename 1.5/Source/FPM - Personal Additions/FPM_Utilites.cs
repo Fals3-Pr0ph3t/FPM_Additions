4e3bf2fd20f4fb68a914b4fd462790162a380133
Fals3-Pr0ph3t <66283967+Fals3-Pr0ph3t@users.noreply.github.com>
2024-08-25 09:21:45 -0500

    Moved folders
    Moving folders to align with expected results when cloning from the repository.

---
+++ 1.5/Source/FPM - Personal Additions/FPM_Utilites.cs
@@ -0,0 +1,19 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using System.Text;
+using Verse;
+
+namespace FPM_Additions
+{
+
+
+   
+    public static class FPM_Utilites
+    {
+
+
+        
+    }
+
+
+}
